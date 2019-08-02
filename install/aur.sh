78e5d8006c0bc81a7dfef84b7eddb8df74530d24
Vivek Kalyan <hello@vivekkalyan.com>
2019-08-02 20:19:52 +0800

    [arch]: Add AUR repos list

---
+++ install/aur.sh
@@ -0,0 +1,11 @@
+dropbox
+## Prevent automatic updates
+# $ rm -rf ~/.dropbox-dist
+# $ install -dm0 ~/.dropbox-dist
+## Start with systemctl
+# $ systemctl enable dropbox@vivek
+# $ systemctl start dropbox@vivek
+openfortivpn
+python-keepmenu-git
+python-pyuserinput-git
+# $ ln -sfv arch/keepmenu.conf ~/.config/keepmenu/config.ini
