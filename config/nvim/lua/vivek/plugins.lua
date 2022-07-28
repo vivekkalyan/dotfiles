18cfe975b50f1b9deac1bbb306cdce35d85534f5
Vivek Kalyan <hello@vivekkalyan.com>
2022-07-29 02:12:21 +0800

    [nvim]: Add autopairs plugin

--- config/nvim/lua/vivek/plugins.lua
+++ config/nvim/lua/vivek/plugins.lua
@@ -71,6 +71,9 @@ return packer.startup(function(use)
   -- treesitter
   use { 'nvim-treesitter/nvim-treesitter' }
 
+  -- autopairs
+  use "windwp/nvim-autopairs"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
