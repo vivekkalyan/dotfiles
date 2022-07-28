e8da903fc6cbda58e471b6e6463e194c5028713c
Vivek Kalyan <hello@vivekkalyan.com>
2022-07-28 21:01:04 +0800

    [nvim]: Change from nvim-lsp-installer to mason

--- config/nvim/lua/vivek/lsp.lua
+++ config/nvim/lua/vivek/lsp.lua
@@ -1,9 +1,10 @@
-require("nvim-lsp-installer").setup({
+require("mason").setup()
+require("mason-lspconfig").setup({
     automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
     ui = {
         icons = {
             server_installed = "✓",
-            server_pending = "➜",
+            server_pending = ">",
             server_uninstalled = "✗"
         }
     }
