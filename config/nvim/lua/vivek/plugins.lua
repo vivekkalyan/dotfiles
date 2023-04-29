dc32961f23eb6a732307cf4986753871dac80a94
Vivek Kalyan <hello@vivekkalyan.com>
2023-04-30 02:31:29 +0800

    [nvim]: Add comment

--- config/nvim/lua/vivek/plugins.lua
+++ config/nvim/lua/vivek/plugins.lua
@@ -78,6 +78,9 @@ return packer.startup(function(use)
   -- autopairs
   use "windwp/nvim-autopairs"
 
+  -- comment
+  use 'echasnovski/mini.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
