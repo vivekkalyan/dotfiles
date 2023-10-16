33725b11d1d97460a5c9959e7940faf296e6d4a5
Vivek Kalyan <hello@vivekkalyan.com>
2023-10-16 18:39:57 +0800

    [nvim]: Add luasnip keymaps

--- config/nvim/lua/vivek/plugins/cmp.lua
+++ config/nvim/lua/vivek/plugins/cmp.lua
@@ -14,6 +14,23 @@ return {
     local cmp = require("cmp")
 
     local luasnip = require("luasnip")
+    vim.keymap.set({ "i", "s" }, "<C-k>", function()
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      end
+    end, { silent = true })
+
+    vim.keymap.set({ "i", "s" }, "<C-j>", function()
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      end
+    end, { silent = true })
+
+    vim.keymap.set({ "i" }, "<C-l>", function()
+      if luasnip.choice_active() then
+        luasnip.change_choice()
+      end
+    end, { silent = true })
 
     local lspkind = require("lspkind")
 
