fd796c3e25e4777710bab1be684610d588bd42e2
Vivek Kalyan <hello@vivekkalyan.com>
2023-10-28 00:32:26 +0800

    [nvim]: Add unimpaired.vim plugin

--- config/nvim/lua/vivek/plugins/whichkey.lua
+++ config/nvim/lua/vivek/plugins/whichkey.lua
@@ -1,9 +1,15 @@
 return {
   "folke/which-key.nvim",
+  dependencies = { "afreakk/unimpaired-which-key.nvim" },
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  opts = {},
+  config = function()
+    local whichkey = require("which-key")
+    local unimpaired_whichkey = require("unimpaired-which-key")
+    whichkey.register(unimpaired_whichkey.normal_mode)
+    whichkey.register(unimpaired_whichkey.normal_mode_and_visual_mode, { mode = { "n", "v" } })
+  end,
 }
