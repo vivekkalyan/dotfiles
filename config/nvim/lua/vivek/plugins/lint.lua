0949be6945eb8fc2bf4ac25e19296a841855328a
Vivek Kalyan <hello@vivekkalyan.com>
2024-03-01 11:29:11 +0800

    [nvim]: Remove lint autocmd to enable/disable

--- config/nvim/lua/vivek/plugins/lint.lua
+++ config/nvim/lua/vivek/plugins/lint.lua
@@ -12,14 +12,5 @@ return {
       typescriptreact = { "eslint_d" },
       python = { "ruff" },
     }
-
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
   end,
 }
