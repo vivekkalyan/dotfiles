8048c5da2778bb689ea29ec0b43e67806d483008
Vivek Kalyan <hello@vivekkalyan.com>
2024-03-11 15:37:06 +0800

    [nvim]: Define delimited colors

--- config/nvim/lua/vivek/plugins/colorscheme.lua
+++ config/nvim/lua/vivek/plugins/colorscheme.lua
@@ -18,11 +18,16 @@ return {
           all = function(colors)
             -- make StatusLine and StatusLineNC be easier to read
             -- make IncSearch and CurSearch more pastel
+            -- define colors for delimited plugin
             return {
               StatusLine = { bg = "#87875f", fg = "#1c1c1c" },
               StatusLineNC = { bg = "#444444", fg = "#87875f" },
               IncSearch = { bg = "#89b4fa", style = { "bold" } },
               CurSearch = { bg = "#f5c2e7", style = { "bold" } },
+              DelimitedError = { bg = colors.red },
+              DelimitedWarn = { bg = colors.red },
+              DelimitedInfo = { bg = colors.red },
+              DelimitedHint = { bg = colors.red },
             }
           end,
         },
