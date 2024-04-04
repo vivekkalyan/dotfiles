19e9ac4b624e5e41be77c4c2d54c293c60d878ed
Vivek Kalyan <hello@vivekkalyan.com>
2024-04-04 17:24:33 +0800

    [nvim]: Install jsregexp for snippet transforms

--- config/nvim/lua/vivek/plugins/cmp.lua
+++ config/nvim/lua/vivek/plugins/cmp.lua
@@ -5,7 +5,7 @@ return {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
     "hrsh7th/cmp-cmdline", -- cmdline completions
-    "L3MON4D3/LuaSnip", -- snippet engine
+    { "L3MON4D3/LuaSnip", build = "make install_jsregexp" }, -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
     "onsails/lspkind.nvim", -- vs-code like pictograms
