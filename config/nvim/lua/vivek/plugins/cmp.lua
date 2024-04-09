26b5c39d6c1c081998be69c62f1febf9df5307fb
Vivek Kalyan <hello@vivekkalyan.com>
2024-04-09 15:59:00 +0800

    [nvim]: Add cmp-calc for calculations

--- config/nvim/lua/vivek/plugins/cmp.lua
+++ config/nvim/lua/vivek/plugins/cmp.lua
@@ -5,6 +5,7 @@ return {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
     "hrsh7th/cmp-cmdline", -- cmdline completions
+    "hrsh7th/cmp-calc", -- calculator completions
     { "L3MON4D3/LuaSnip", build = "make install_jsregexp" }, -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
@@ -66,6 +67,7 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
+        { name = "calc" }, -- calculator
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
