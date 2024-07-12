77078ad65131068a1085de1f559f94e71559617e
Vivek Kalyan <hello@vivekkalyan.com>
2024-07-13 02:35:34 +0800

    [nvim]: Change to builtin commenting

--- config/nvim/lua/vivek/plugins/treesitter.lua
+++ config/nvim/lua/vivek/plugins/treesitter.lua
@@ -56,6 +56,13 @@ return {
       require("ts_context_commentstring").setup({
         enable_autocmd = false,
       })
+      -- override the Neovim internal get_option function which is called whenever the commentstring is requested
+      local get_option = vim.filetype.get_option
+      vim.filetype.get_option = function(filetype, option)
+        return option == "commentstring"
+          and require("ts_context_commentstring.internal").calculate_commentstring()
+          or get_option(filetype, option)
+      end
     end,
   },
 }
