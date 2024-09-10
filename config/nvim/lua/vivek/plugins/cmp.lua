1d7ebca567ea17d62f433136e2c283211dedb46c
Vivek Kalyan <hello@vivekkalyan.com>
2024-09-11 02:47:20 +0800

    [nvim]: Change keyamp to accept completion to C-y

--- config/nvim/lua/vivek/plugins/cmp.lua
+++ config/nvim/lua/vivek/plugins/cmp.lua
@@ -88,7 +88,7 @@ return {
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
