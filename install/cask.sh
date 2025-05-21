607c961c47dcc80f7bbb22d44050a9736912751c
Vivek Kalyan <hello@vivekkalyan.com>
2025-05-21 15:24:03 +0800

    [mac]: Add cask apps

--- install/cask.sh
+++ install/cask.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
 apps=(
-  alacritty
+  alacritty # can remove if ghostty works well
   bruno
+  cursor
   docker
   flux
   font-meslo-lg-nerd-font
   ghostty
   karabiner-elements
+  keepassxc
   linear-linear
+  macwhisper
+  msty
   notion
   obsidian
+  raycast
   spotify
   telegram
+  todoist
+  vlc
+  zoom
 )
 
 brew cask install "${apps[@]}"
