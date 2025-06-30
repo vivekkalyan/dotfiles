b702f953299e31d05aca95aa189c186ac42bb916
Vivek Kalyan <hello@vivekkalyan.com>
2025-06-30 15:56:42 +0800

    [nvim]: Add bigfile, indent, notifier, scroll, words plugins

---
+++ config/nvim/lua/vivek/plugins/snacks.lua
@@ -0,0 +1,25 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    bigfile = { enabled = true },
+    indent = {
+      animate = {
+        duration = { step = 15, total = 150 },
+        easing = "linear",
+      },
+    },
+    notifier = { enabled = true },
+    scroll = {
+      animate = {
+        duration = { step = 15, total = 150 },
+        easing = "linear",
+      },
+      animate_repeat = {
+        delay = 100, -- delay in ms before using the repeat animation
+        duration = { step = 5, total = 10 },
+        easing = "linear",
+      },
+    },
+    words = { enabled = true },
+  },
+}
