106ea91c0c6f0cff46f9de77c7730220532d6587
Vivek Kalyan <hello@vivekkalyan.com>
2025-06-30 12:40:32 +0800

    [nvim]: Update venv-selector config

--- config/nvim/lua/vivek/plugins/venv-selector.lua
+++ config/nvim/lua/vivek/plugins/venv-selector.lua
@@ -10,12 +10,11 @@ return {
     -- Your options go here
     -- name = "venv",
     -- auto_refresh = false
+    notify_user_on_venv_activation = true,
   },
-  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  ft = "python",
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
-    { "<leader>vs", "<cmd>VenvSelect<cr>", { desc = "Select venv" } },
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Select cached venv " },
+    { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" },
   },
 }
