48736783eb86295de3fdfe118df1707a96fd0ea9
Vivek Kalyan <hello@vivekkalyan.com>
2025-08-22 19:42:14 +0800

    [mac]: Update cask packages

--- install/cask.sh
+++ install/cask.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 apps=(
-  alacritty # can remove if ghostty works well
-  bruno
   claude
-  cursor
   docker
   flux
   font-meslo-lg-nerd-font
@@ -16,13 +13,14 @@ apps=(
   msty
   notion
   obsidian
-  orion
   raycast
+  slack
   spotify
   telegram
   todoist
   vlc
+  zen
   zoom
 )
 
-brew cask install "${apps[@]}"
+brew install --cask "${apps[@]}"
