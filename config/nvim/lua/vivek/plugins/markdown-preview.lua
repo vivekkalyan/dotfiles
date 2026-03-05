14ad055d4397e430f546876f3a16f364a53da9cf
Vivek Kalyan <hello@vivekkalyan.com>
2026-03-05 11:59:42 -0800

    [nvim]: Harden MarkdownPreview install recovery

--- config/nvim/lua/vivek/plugins/markdown-preview.lua
+++ config/nvim/lua/vivek/plugins/markdown-preview.lua
@@ -3,7 +3,11 @@ return {
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
   build = function()
-    vim.fn["mkdp#util#install"]()
+    vim.fn["mkdp#util#install_sync"](1)
+  end,
+  config = function()
+    -- Self-heal in case plugin files exist but prebuilt runtime binary is missing.
+    vim.fn["mkdp#util#install_sync"](1)
   end,
   keys = {
     {
